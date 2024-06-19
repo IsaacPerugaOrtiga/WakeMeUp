260edc1455d910be4095fb16fcf5d03c4a59a97a
IsaacPerugaOrtiga <isaacperuga03@gmail.com>
2024-06-19 22:11:05 +0200

    Crear objeto en la clase Alarms con su tipo correspondiente

--- app/src/main/java/com/isaacpodev/wakemeup/models/Alarm.kt
+++ app/src/main/java/com/isaacpodev/wakemeup/models/Alarm.kt
@@ -1,6 +1,8 @@
 package com.isaacpodev.wakemeup.models
 
 
+import io.realm.kotlin.ext.realmListOf
+import io.realm.kotlin.types.RealmList
 import io.realm.kotlin.types.RealmObject
 import io.realm.kotlin.types.annotations.PrimaryKey
 import org.mongodb.kbson.ObjectId
@@ -9,7 +11,7 @@ class Alarm(): RealmObject {
     @PrimaryKey
     var _id : ObjectId = ObjectId()
     var alarm: String = ""
-    var days: String = ""
+    var days: RealmList<String> =  realmListOf()
     var isActivate: Boolean = false
     var owner_id: String = ""
  constructor(ownerId: String = ""): this(){
